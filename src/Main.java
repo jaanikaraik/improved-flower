57d41c8989c81c5b5521d3cac6718b18e13c71aa
Jaanika Raik <jaraik@ttu.ee>
2018-08-07 21:03:33 +0300

    juhuslikkus

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,23 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class Main extends Application {
-    private final int WIDTH = 400;
-    private final int HEIGHT = 300;
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
+    private Random random = new Random();
+
+    private int radius1 = random.nextInt(30) + 10;
+    private int radius2 = random.nextInt(30) + 10;
+
+    private int x1 = random.nextInt(400) + 100;
+    private int y1 = random.nextInt(400) + 100;
+    private int x2 = random.nextInt(400) + 100;
+    private int y2 = random.nextInt(400) + 100;
+
+    private int colorInt1 = random.nextInt(5);
+    private int colorInt2 = random.nextInt(5);
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -16,22 +30,38 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Peenar");
         final Flower flower = new FlowerBuilder()
-                .setRadius(30)
-                .setCenterX(200)
-                .setCenterY(150)
-                .setColor(Color.PINK)
+                .setRadius(radius1)
+                .setCenterX(x1)
+                .setCenterY(y2)
+                .setColor(generateColor(colorInt1))
                 .createFlower();
         Group root = new Group();
         flower.drawFlowerVariablePetals(root);
         final Flower flower2 = new FlowerBuilder()
-                .setRadius(30)
-                .setCenterX(100)
-                .setCenterY(100)
-                .setColor(Color.ORANGE)
+                .setRadius(radius2)
+                .setCenterX(x2)
+                .setCenterY(y2)
+                .setColor(generateColor(colorInt2))
                 .createFlower();
         flower2.drawFlowerVariablePetals(root);
         final Scene scene = new Scene(root, WIDTH, HEIGHT, Color.LIGHTGREEN);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public Color generateColor(int number) {
+        Color color;
+        if (number == 1) {
+            color = Color.ORANGE;
+        } else if (number == 2) {
+            color = Color.PINK;
+        } else if (number == 3) {
+            color = Color.LIGHTBLUE;
+        } else if (number == 4){
+            color = Color.DARKBLUE;
+        } else {
+            color = Color.ORANGERED;
+        }
+        return color;
     }
 }
